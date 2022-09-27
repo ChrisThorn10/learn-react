ce66000fc212f1c2d692696dc4e9b2ba7328d33e
Christie Thornton <christiethornton@Christies-MacBook-Pro.local>
2022-09-26 18:47:00 -0700

    completing coding of class based react app

---
+++ tutorials/ibas-react-for-beginners/react-todo-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+//component file
+import TodoContainer from "./components/TodoContainer"
+
+const element = <h1>Hello from Create React App</h1>
+
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode>, 
+    document.getElementById("root")
+    )
